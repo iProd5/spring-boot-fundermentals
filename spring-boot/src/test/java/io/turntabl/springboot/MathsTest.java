2830b77f6b9bf4347913d2e63ebf408989bc5599
iProd5 <franklinluther3@gmail.com>
2019-11-20 16:17:31 +0000

    test init

---
+++ spring-boot/src/test/java/io/turntabl/springboot/MathsTest.java
@@ -0,0 +1,16 @@
+package io.turntabl.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathsTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void substract() {
+    }
+}
