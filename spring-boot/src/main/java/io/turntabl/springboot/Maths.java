09fe2b500b4a1bb7f1ce395cb9bf983e49abc864
iProd5 <franklinluther3@gmail.com>
2019-11-20 17:25:36 +0000

    test ongoing

--- spring-boot/src/main/java/io/turntabl/springboot/Maths.java
+++ spring-boot/src/main/java/io/turntabl/springboot/Maths.java
@@ -2,12 +2,12 @@ package io.turntabl.springboot;
 
 public class Maths {
 
-    public Integer add(int numb1, int numb2){
+    public static Integer add(int numb1, int numb2){
         int total = numb1 + numb2;
         return total;
     }
 
-    public Integer substract(int numb1, int numb2){
+    public static Integer substract(int numb1, int numb2){
         int total = numb1 - numb2;
         return total;
     }
--- spring-boot/src/test/java/io/turntabl/springboot/MathsTest.java
+++ spring-boot/src/test/java/io/turntabl/springboot/MathsTest.java
@@ -8,11 +8,20 @@ class MathsTest {
 
     @Test
     void add() {
-
+    int sum =  Maths.add(1,2);
+    assertEquals(3,sum);
     }
 
     @Test
     void substract() {
-
+        int minus = Maths. substract(0,1);
+        assertEquals(-1,minus);
     }
+
+    @Test
+    void ZerosubstractedZero() {
+        int minus = Maths. substract(0,0);
+        assertEquals(0,minus);
+    }
+
 }
